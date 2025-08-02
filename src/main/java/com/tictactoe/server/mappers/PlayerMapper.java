86731328a07836bdd30cdc26b7cf0f7cf130c9bd
NikolaiUnknown <thestonmix@gmail.com>
2025-08-02 12:39:36 +0300

    bugfix

--- src/main/java/com/tictactoe/server/mappers/PlayerMapper.java
+++ src/main/java/com/tictactoe/server/mappers/PlayerMapper.java
@@ -1,6 +1,7 @@
 package com.tictactoe.server.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import com.tictactoe.server.dto.RegisterRequestDto;
@@ -8,6 +9,8 @@ import com.tictactoe.server.models.Player;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PlayerMapper {
-    
+    @Mapping(target = "dateOfRegistration", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "rating", ignore = true)
     Player registerDtoToPlayer(RegisterRequestDto registerRequestDto);
 }
