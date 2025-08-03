89359920ea90a3778ab70248b1e91f7b9ad7bca6
NikolaiUnknown <thestonmix@gmail.com>
2025-08-04 00:13:12 +0300

    remove unnecessary inject from AuthControllerTest

--- src/test/java/com/tictactoe/server/controllers/AuthControllerTest.java
+++ src/test/java/com/tictactoe/server/controllers/AuthControllerTest.java
@@ -54,8 +54,6 @@ public class AuthControllerTest {
     @MockitoBean
     private RefreshTokenService refreshTokenService;
 
-    @InjectMocks
-    private AuthController authController;
 
     @Autowired
     private MockMvc mockMvc;
