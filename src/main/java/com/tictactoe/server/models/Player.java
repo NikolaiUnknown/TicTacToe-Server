0e613082da026ac94031084aaffaa389b38bc16b
NikolaiUnknown <thestonmix@gmail.com>
2025-08-11 12:18:48 +0300

    add game entity fields, eq&hc

--- src/main/java/com/tictactoe/server/models/Player.java
+++ src/main/java/com/tictactoe/server/models/Player.java
@@ -1,11 +1,13 @@
 package com.tictactoe.server.models;
 
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -33,6 +35,58 @@ public class Player {
 
     public Player(Long id) {
         this.id = id;
+    }
+
+    @OneToMany(mappedBy = "firstPlayer")
+    private List<Game> proposedGames;
+
+    @OneToMany(mappedBy = "secondPlayer")
+    private List<Game> receivedGames;
+
+    @OneToMany(mappedBy = "winner")
+    private List<Game> wonGames;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Player other = (Player) obj;
+        if (nickname == null) {
+            if (other.nickname != null)
+                return false;
+        } else if (!nickname.equals(other.nickname))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (dateOfRegistration == null) {
+            if (other.dateOfRegistration != null)
+                return false;
+        } else if (!dateOfRegistration.equals(other.dateOfRegistration))
+            return false;
+        if (rating == null) {
+            if (other.rating != null)
+                return false;
+        } else if (!rating.equals(other.rating))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nickname == null) ? 0 : nickname.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((dateOfRegistration == null) ? 0 : dateOfRegistration.hashCode());
+        result = prime * result + ((rating == null) ? 0 : rating.hashCode());
+        return result;
     }
 
     
